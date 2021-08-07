3adb4983120cdd73637929e879f34e790ca68278
gabriel-antonelli <gabrielantonelli25@gmail.com>
2021-08-06 22:51:23 -0300

    verify alert component

--- components/alert.js
+++ components/alert.js
@@ -3,8 +3,6 @@ import {useEffect, useState} from "react";
 export function Alert(props) {
     const [show, setShow] = useState(false);
 
-    console.log(props)
-
     useEffect(() => {
         setShow(props.show);
     }, [props.show]);
@@ -14,15 +12,11 @@ export function Alert(props) {
             props.func()
         }, 6000)
 
-    let style = "close cursor-pointer flex items-center justify-center w-full p-2 md:h-24 h-18 rounded shadow-lg text-white " + "bg-" + props.color + "-500"
-    
-    console.log(style)
-    
     return (
         show && (
             <div className="flex justify-center items-center">
             <div className="flex items-center justify-center fixed mt-40 md:w-full md:bottom-0 md:right-0 md:m-8 sm:max-w-sm z-40">
-                <label className={style}>
+                <label className={`bg-${props.color}-500 close cursor-pointer flex items-center justify-center w-full p-2 md:h-24 h-18 rounded shadow-lg text-white`}>
                     {props.label}
                     <button
                         className="hidden"
