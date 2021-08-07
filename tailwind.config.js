b8dc58ed725b4d79599f1c7b098f6c26a088435b
gabriel-antonelli <gabrielantonelli25@gmail.com>
2021-08-06 23:05:08 -0300

    verify alert component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: {
+    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    options: {
+      safelist: ['bg-red-500', 'bg-blue-500']
+    }
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
