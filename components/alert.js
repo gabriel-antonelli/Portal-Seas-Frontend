1b0b67a01395d650813c7ab4f624c15fd79d1bc8
gabriel-antonelli <gabrielantonelli25@gmail.com>
2021-08-06 22:43:06 -0300

    verify alert component

--- components/alert.js
+++ components/alert.js
@@ -3,17 +3,21 @@ import {useEffect, useState} from "react";
 export function Alert(props) {
     const [show, setShow] = useState(false);
 
+    console.log(props)
+
     useEffect(() => {
         setShow(props.show);
     }, [props.show]);
 
-    if(show)
+    if(props.show)
         setTimeout(() => {
             props.func()
         }, 6000)
 
     let style = "close cursor-pointer flex items-center justify-center w-full p-2 md:h-24 h-18 rounded shadow-lg text-white " + "bg-" + props.color + "-500"
-
+    
+    console.log(style)
+    
     return (
         show && (
             <div className="flex justify-center items-center">
