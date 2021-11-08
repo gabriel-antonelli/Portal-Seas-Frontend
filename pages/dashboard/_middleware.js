3b30825225ca4766e70a19654287c9ff52116d29
gabriel-antonelli <gabrielantonelli25@gmail.com>
2021-11-08 01:15:39 -0300

    chore: add middleware, refactor auth

---
+++ pages/dashboard/_middleware.js
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server';
+
+export default async function middleware(req) {
+	const { ok } = await fetch(process.env.NEXT_PUBLIC_API_URL + 'cor', {
+		headers: { Authorization: req.cookies[process.env.NEXT_PUBLIC_TOKEN] },
+	});
+	if (ok) {
+		return NextResponse.next();
+	} else if (req.nextUrl.href !== '/login/loginPage') {
+		return NextResponse.redirect('/login/loginPage');
+	}
+}
